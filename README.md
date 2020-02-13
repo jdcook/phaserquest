ed3815d9a068664d796c342833fe9b171dbc1a72
jdcook <jaredc35@gmail.com>
2020-02-12 21:36:49 -0600

    cleaning up, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # phaserquest
-A first attempt at making a clean typescript, babel-compiled dev setup for a Phaser js project
+A first attempt at making a clean typescript, webpacked dev setup for a Phaser js project
+
+## Setup
+Make sure you have [Node, NPM](https://nodejs.org/en/download/), and [Yarn](https://classic.yarnpkg.com/en/docs/install#windows-stable) installed.
+
+Run "yarn" to install dependencies.
+
+## Running project
+
+"yarn dev" to webpack and start a webpack server. Open up localhost:8080 in your browser to load the game.
+
+"yarn build" to output a production build.
+
--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -23,14 +23,14 @@ export class MainScene extends Phaser.Scene {
         this.square.x += 10 * this.direction.x;
         this.square.y += 10 * this.direction.y;
 
-        if (this.square.x >= this.game.scale.width + this.square.width / 2) {
+        if (this.square.x >= this.game.scale.width - this.square.width / 2) {
             this.direction.x = -1;
-        } else if (this.square.x <= 0) {
+        } else if (this.square.x <= this.square.width / 2) {
             this.direction.x = 1;
         }
-        if (this.square.y >= this.game.scale.height + this.square.height / 2) {
+        if (this.square.y >= this.game.scale.height - this.square.height / 2) {
             this.direction.y = -1;
-        } else if (this.square.y <= 0) {
+        } else if (this.square.y <= this.square.height / 2) {
             this.direction.y = 1;
         }
     }
