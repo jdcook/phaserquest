a4eb59a8a3353acb8e7829760d38de520c9981eb
jdcook <jaredc35@gmail.com>
2020-02-13 19:05:43 -0600

    added added player

---
+++ src/entities/player.ts
@@ -0,0 +1,21 @@
+export class Player extends Phaser.GameObjects.GameObject {
+    arrowKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+    sprite: Phaser.Physics.Arcade.Sprite;
+
+    constructor(scene) {
+        super(scene, "player");
+        this.scene.add.existing(this);
+        this.sprite = this.scene.physics.add.sprite(100, 100, "player");
+        this.arrowKeys = this.scene.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        if(this.arrowKeys.left.isDown) {
+            this.sprite.setVelocityX(-500);
+        } else if(this.arrowKeys.right.isDown) {
+            this.sprite.setVelocityX(500);
+        } else {
+            this.sprite.setVelocityX(0);
+        }
+    }
+}
